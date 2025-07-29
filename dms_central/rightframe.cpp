23d38c794e238d5a7d23b910ea4eb273984fc5f2
HAN_SH <hansc59220@gmail.com>
2025-07-30 00:46:25 +0900

    plusscroll

--- dms_central/rightframe.cpp
+++ dms_central/rightframe.cpp
@@ -1,11 +1,16 @@
 #include "rightframe.h"
 #include "ui_rightframe.h"
 
+
 RightFrame::RightFrame(MainWindow* mainWindow, QWidget *parent)
     : mainWindow(mainWindow), QFrame(parent)
     , ui(new Ui::RightFrame)
 {
     ui->setupUi(this);
+
+
+
+    scrollArea = new QScrollArea(this);
 }
 
 RightFrame::~RightFrame()
@@ -20,3 +25,56 @@ RightFrame::~RightFrame()
 
 이벤트, 알람 탭 버튼 - 탭, 내용 모두 스위치
 */
+
+
+
+/*
+RightFrame::RightFrame(MainWindow* mainWindow, QWidget *parent)
+    : mainWindow(mainWindow), QFrame(parent)
+    , ui(new Ui::RightFrame)
+{
+    ui->setupUi(this);
+
+    scrollArea = new QScrollArea(this);
+    widget_1 = new QWidget(scrollArea);
+
+    contentWidget->setLayout(layout);
+    scrollArea->setWidgetResizable(true);
+    scrollArea->setWidget(contentWidget);
+    setCentralWidget(scrollArea);
+
+    // 예시: 1초마다 새 위젯 추가
+    QTimer *timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &RightFrame::addNewWidget);
+    timer->start(1000);  // 1초마다
+}
+
+void RightFrame::addNewWidget() {
+    // 새 라벨 생성
+    QLabel *label = new QLabel(QString("New Widget #%1").arg(++count));
+    layout->addWidget(label);
+
+    // 스크롤 맨 아래로 이동
+    QTimer::singleShot(0, [=]() {
+        QScrollBar *bar = scrollArea->verticalScrollBar();
+        bar->setValue(bar->maximum());
+    });
+}
+*/
+
+void RightFrame::addNewWidget()
+{
+    //    CustomScrollArea* scrollArea = new QScrollArea;
+    scrollArea->resize(320, 240);
+    scrollArea->show();
+
+    // 1초마다 새로운 버튼 추가
+    QTimer* timer = new QTimer;
+    QObject::connect(timer, &QTimer::timeout, [=]() {
+        QPushButton* btn = new QPushButton("New Item");
+        btn->resize(300, 40);
+        //   scrollArea->addNewWidget(btn);
+    });
+    timer->start(1000);  // 1초마다 추가
+
+}
